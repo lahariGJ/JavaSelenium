dd91bc6be284930b3da571bd8996493ecb43b1f2
lahari <laharigj1995@gmail.com>
2020-06-17 12:39:28 -0400

    added comments_v2

--- sample/src/samplejson/sample.java
+++ sample/src/samplejson/sample.java
@@ -4,7 +4,7 @@ import java.io.FileWriter;
 import java.io.IOException;
 import org.json.JSONArray;
 import org.json.JSONObject;
-
+// create json mutiple files with key 
 public class sample {
 
 	public static void main(String[] args) throws IOException {
